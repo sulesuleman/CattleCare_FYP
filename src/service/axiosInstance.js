4c008eb29b1839da2177f942e75aef3535ca4390
usmanmalik321 <68661404+usmanmalik321@users.noreply.github.com>
2021-12-02 03:10:36 +0500

    changed backend url

--- src/service/axiosInstance.js
+++ src/service/axiosInstance.js
@@ -4,7 +4,7 @@ const server = "https://cattlecare-api.herokuapp.com";
 const local = "https://869e-101-50-108-2.ngrok.io";
 
 const axiosInstance = axios.create({
-  baseURL: local,
+  baseURL: server,
 });
 
 export default axiosInstance;
